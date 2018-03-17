15402f5578507626b0e3e3ceb4b995f90880513a
Frank Bennett <biercenator@gmail.com>
2018-03-17 12:53:16 +0900

    Comment out vendor autoload

--- lib/bootstrap.inc.php
+++ lib/bootstrap.inc.php
@@ -1,6 +1,6 @@
 <?php
 define('ROOT_DIR', __DIR__ . '/..');
-require ROOT_DIR . '/vendor/autoload.php';
+// require ROOT_DIR . '/vendor/autoload.php';
 require ROOT_DIR . '/config.inc.php';
 require __DIR__ . '/ClientDownloads.inc.php';
 
